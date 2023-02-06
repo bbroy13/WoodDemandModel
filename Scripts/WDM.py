f096f10f4295fed1e07732ee83646022ff1c004f
bbroy13 <89233505+bbroy13@users.noreply.github.com>
2023-02-06 14:08:37 -0800

    Updated the input data file and code to read the data file

---
+++ Scripts/WDM.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 15:54:58 2023
+
+@author: bbroy
+"""
+import os
+import numpy as np
+import time
+import datetime
+import scipy.io
+import scipy.stats   
+import matplotlib.pyplot as plt    
+import pandas as pd
+import shutil 
+import uuid
+import xlrd
+
+
+"""
+Set file path
+"""    
+
+from pathlib import Path
+
+# `cwd`: current directory
+cwd = Path('C:\\Users\\bbroy\\OneDrive - UBC\\PhD poposal\\Dynamic MFA of buildings\\demand model for wood in construction sector\\WoodDemandModel')
+
+Project_MainPath = cwd
+Input_Data        = 'InData.xls'
+Path_Data   = Path.joinpath(Project_MainPath ,'Data')
+Path_Results = Path.joinpath(Project_MainPath ,'Results')
+Path_Script = Path.joinpath(Project_MainPath ,'Scripts')
+Path_GeneralResults = Path.joinpath(Project_MainPath ,'General_Results')
+
+"""
+Rade the configuration file
+"""
+
+Project_DataFileName = Input_Data
+Project_DataFilePath = Path.joinpath(Path_Data , Project_DataFileName)
+Project_DataFile_WB  = xlrd.open_workbook(Project_DataFilePath)
+Project_Configsheet  = Project_DataFile_WB.sheet_by_name('Model_config')
+
+Model_name= Project_Configsheet.cell_value(0,1)
+scriptConfig={}
+
+for m in range (3,13):
+    scriptConfig [Project_Configsheet.cell_value(m,0)]= Project_Configsheet.cell_value(m,1)
+
+
+       
+       
+    
